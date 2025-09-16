e709c8979103b77dbfb7831c3e41769d6c7ff23a
Chidera <pyjamel224@gmail.com>
2025-09-16 09:23:38 -0700

    Fixed admin

--- giftapp/admin.py
+++ giftapp/admin.py
@@ -11,9 +11,14 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(CoinPurchase)
 class CoinPurchaseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user__username', 'amount')
+    list_display = ('id', 'coinpurchase_user_username', 'amount')
     search_fields = ('user__username',)
-    list_filter = ('amount', 'user__username')
+    list_filter = ('amount', 'user__username', 'coins')
+
+    def coinpurchase_user_username(self, obj):
+        return obj.user.username
+    
+    coinpurchase_user_username.short_description = 'User'
 
 
 @admin.register(Gift)
@@ -24,16 +29,36 @@ class GiftAdmin(admin.ModelAdmin):
 
 @admin.register(GiftTransaction)
 class GiftTransactionAdmin(admin.ModelAdmin):
-    list_display = ("id", "gifter", "recipient", "gift__name", "claimed_by", "created_at")
+    list_display = ("id", "gifter", "recipient", "gifttransaction_gift_name", "claimed_by", "created_at")
     list_filter = ("created_at",)
     search_fields = ("gifter__username", "recipient__username")
+
+    def gifttransaction_gift_name(self, obj):
+        return obj.gift.name
+
+    def gifttransaction_gifter_username(self, obj):
+        return obj.gifter.username
+    
+    gifttransaction_gift_name.short_description = 'Gift Name'
+    gifttransaction_gifter_username.short_description = 'Gifter'
+
 
 
 @admin.register(FastestFingerClaim)
 class FastestFingerClaimAdmin(admin.ModelAdmin):
-    list_display = ("id", "gift_transaction__gift__name", "user__username", "claim_time", "reaction_time_ms")
+    list_display = ("id", "ffclaim_gifttransaction_gift_name", "ffclaim_user_username", "claim_time", "reaction_time_ms")
     list_filter = ("gift_transaction",)
     search_fields = ("reaction_time_ms",)
+
+    def ffclaim_gifttransaction_gift_name(self, obj):
+        return obj.gift_transaction.gift.name
+
+    def ffclaim_user_username(self, obj):
+        return obj.user.username
+    
+    ffclaim_gifttransaction_gift_name.short_description = 'Gift Name'
+    ffclaim_user_username.short_description = 'User'
+
 
 
 @admin.register(ProfilePicture)
