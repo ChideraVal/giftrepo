06f1e0ba3828cdb919910564ab0a9affc5975c01
Valentine <valentine@mail.com>
2025-11-20 08:17:45 -0800

    fix slide content 5

--- giftapp/views.py
+++ giftapp/views.py
@@ -750,6 +750,6 @@ def all_gifts(request):
         
         ).order_by('seconds_before_drop', 'seconds_before_expire')
 
-    return render(request, "all_gifts_partial.html", {"gift_transactions": gift_transactions})
-    # return render(request, "all_gifts.html")
+    # return render(request, "all_gifts_partial.html", {"gift_transactions": gift_transactions})
+    return render(request, "scroll.html")
 
